1b616aae841e7662782675786a49210a75eeb5cd
Marton Kovacs <narancsjuice@gmail.com>
2022-07-12 16:57:36 +0200

    Add more backend code
    Added default categories to categories table.
    Added functions for querying for the following:
    -view categories
    -sum income
    -sum spendings
    -sum savings
    
    Changed income to decimal value.
    
    GH-4

--- backend.py
+++ backend.py
@@ -16,14 +16,12 @@ def connect():
                 "(id INTEGER PRIMARY KEY,"
                 " currency TEXT NOT NULL,"
                 " symbol TEXT NOT NULL,"
-                " income INTEGER)")
+                " income DECIMAL(10,2))")
 
     # inserting default value
-    cur.execute("INSERT INTO config(id, currency, symbol, income)"
+    cur.execute("INSERT INTO config (id, currency, symbol, income)"
                 " SELECT 1, 'Hungarian Forint', 'HUF', 350000"
                 " WHERE NOT EXISTS (SELECT * FROM config WHERE id = 1)")
-
-    #TODO: insert default into config or create table with default values
 
     # spendings categories table
     cur.execute("CREATE TABLE IF NOT EXISTS"
@@ -31,6 +29,34 @@ def connect():
                 "(id INTEGER PRIMARY KEY,"
                 " category TEXT,"
                 " type INTEGER)")
+
+    # list of categories to insert into categories table
+    categories_list = [(1, "Salary", 1),
+                       (2, "Bonus", 1),
+                       (3, "Gift", 1),
+                       (4, "Government Assistance", 1),
+                       (5, "Housing", 2),
+                       (6, "Groceries", 2),
+                       (7, "Utilities", 2),
+                       (8, "Transportation", 2),
+                       (9, "Healthcare", 2),
+                       (10, "Insurance", 2),
+                       (11, "Household items", 2),
+                       (12, "Clothing", 2),
+                       (13, "Personal", 2),
+                       (14, "Entertainment", 2),
+                       (15, "Education", 2),
+                       (16, "Gift", 2),
+                       (17, "Debt", 2),
+                       (18, "Retirement", 2),
+                       (19, "Emergency funds", 3),
+                       (20, "Long-term savings", 3),
+                       (21, "Fun savings", 3)]
+
+    for category in categories_list:
+        cur.execute("INSERT INTO categories (id, category, type)"
+                    " SELECT ?, ?, ?"
+                    " WHERE NOT EXISTS (SELECT * FROM categories WHERE id = ?)", (category[0], category[1], category[2], category[0]))
 
     #TODO: use dates for lines
 
@@ -72,6 +98,15 @@ def view_config():
     return rows
 
 
+def view_categories():
+    con = sqlite3.connect("personal_finance.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM categories")
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+
 def search_line(amount="", category_id="", description=""):
     con = sqlite3.connect("personal_finance.db")
     cur = con.cursor()
@@ -104,6 +139,33 @@ def modify_config(currency, symbol, income):
     cur.execute("UPDATE config SET currency=?, symbol=?, income=? WHERE id=1", (currency, symbol, income))
     con.commit()
     con.close()
+
+
+def sum_income():
+    con = sqlite3.connect("personal_finance.db")
+    cur = con.cursor()
+    cur.execute("SELECT sum(amount) FROM account LEFT JOIN categories ON category_id=categories.id WHERE type=1")
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+
+def sum_spendings():
+    con = sqlite3.connect("personal_finance.db")
+    cur = con.cursor()
+    cur.execute("SELECT sum(amount) FROM account LEFT JOIN categories ON category_id=categories.id WHERE type=2")
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+
+def sum_savings():
+    con = sqlite3.connect("personal_finance.db")
+    cur = con.cursor()
+    cur.execute("SELECT sum(amount) FROM account LEFT JOIN categories ON category_id=categories.id WHERE type=3")
+    rows = cur.fetchall()
+    con.close()
+    return rows
 
 #TODO: add/remove/update categories backend code
 
